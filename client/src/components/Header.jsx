e3898af6a9907366700bf775dbfe5f2c6162eefc
Sayed Sofiyan <your.email@example.com>
2024-09-20 16:39:04 +0530

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,9 +7,9 @@ import {FaMoon} from "react-icons/fa"
 function Header() {
     const path=useLocation().pathname
   return (
-    <Navbar className="border-b-2">
+    <Navbar className="border-b-2 p-3 ">
       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+        <span className="px-2 py-1  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Sufiyan's
         </span>
         Blog
@@ -29,13 +29,13 @@ function Header() {
 
         <div className="flex gap-2 md:order-2 ">
 
-    <Button className="w-12 h-10 hidden sm:inline "   color="gray" pill >
+    <Button className="w-12 h-10  hidden sm:inline "   color="gray" pill >
         <FaMoon />
     </Button>
 
     <Link to="/sign-in">
 
-        <Button gradientDuoTone="purpleToBlue" >Sign In</Button>
+        <Button  className="" gradientDuoTone="purpleToBlue" outline >Sign In</Button>
 
     </Link>
     <Navbar.Toggle/>
