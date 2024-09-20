625626084c0a676103f74aad28cbb77551571acb
Sayed Sofiyan <your.email@example.com>
2024-09-20 17:00:50 +0530

    DB

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
 
-const app=express()
+const app = express();
 
+////////////////////////////////////////////////
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database Connected !!!");
+  })
+  .catch((err) => {
+    console.log(` Database connection Failed ${err}`);
+  });
+/////////////////////////////////////////////////
 
-const port=3000
+const port = 3000;
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}!!`);
-    
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}!!`);
+});
