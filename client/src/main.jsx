2c5ce4893cecdcc2a9bf50908febd6680b144672
Sayed Sofiyan <your.email@example.com>
2024-09-23 17:13:30 +0530

    Darkmode func

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,15 @@ import './index.css'
 import { persistor, store } from './redux/store.js'
 import {Provider} from "react-redux"
 import { PersistGate } from 'redux-persist/integration/react'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+      <ThemeProvider>
      <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
-
+</ThemeProvider>
   </Provider>
   
 )
